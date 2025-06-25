dd864747a6e5840f4881fa89cd66453f3a6b7b30
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2025-06-25 19:18:35 +0300

    refactor(texts) - create i18n constant file

--- src/components/sections/AboutMe/AboutMe.tsx
+++ src/components/sections/AboutMe/AboutMe.tsx
@@ -1,24 +1,22 @@
-import classNames from 'classnames'
+import { i18n } from '@/constants/texts'
 
 import { Heading, Subheading, Text } from '@/components/UI'
 
 import styles from './AboutMe.module.scss'
 
-const AboutMe = ({ className }: { className?: string }) => {
+const { aboutMe: aboutMeI18n } = i18n
+
+const AboutMe = () => {
   return (
-    <section className={classNames(styles.aboutMe, className)}>
+    <section className={styles.aboutMe}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <Heading className={styles.name}>Ron Felsenfeld</Heading>
-          <Subheading className={styles.role}>Full Stack Developer</Subheading>
+          <Heading className={styles.name}>{aboutMeI18n.name}</Heading>
+          <Subheading className={styles.role}>{aboutMeI18n.role}</Subheading>
 
-          <Text className={styles.company}>Working at DevBoost</Text>
+          <Text className={styles.company}>{aboutMeI18n.company}</Text>
 
-          <Text className={styles.description}>
-            AI enthusiast and graduate of Coding Academy - A full stack
-            bootcamp. Passionate about building modern web applications and
-            exploring the latest technologies.
-          </Text>
+          <Text className={styles.description}>{aboutMeI18n.description}</Text>
         </div>
       </div>
     </section>
