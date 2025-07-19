e9819633f9fad8992bbf6e391cec9be65b921f14
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2025-07-19 13:03:06 +0300

    refactor(header) - use list component

--- src/components/layouts/AppHeader/AppHeader.tsx
+++ src/components/layouts/AppHeader/AppHeader.tsx
@@ -1,45 +1,57 @@
 import Link from 'next/link'
 
 import { SectionId } from '@/types/section.types'
+import { NavigationLink } from './navigation.types'
 
 import { i18n } from '@/constants/texts.constants'
+
+import List from '@/components/UI/List/List'
 
 import styles from './AppHeader.module.scss'
 
 const { appHeader: appHeaderI18n } = i18n
 
-const navigationItems = [
-  { label: appHeaderI18n.links[SectionId.About], href: `#${SectionId.About}` },
+const navigationLinks: NavigationLink[] = [
   {
+    id: SectionId.About,
+    label: appHeaderI18n.links[SectionId.About],
+    href: `#${SectionId.About}`,
+  },
+  {
+    id: SectionId.Projects,
     label: appHeaderI18n.links[SectionId.Projects],
     href: `#${SectionId.Projects}`,
   },
   {
+    id: SectionId.Education,
     label: appHeaderI18n.links[SectionId.Education],
     href: `#${SectionId.Education}`,
   },
   {
+    id: SectionId.Experiments,
     label: appHeaderI18n.links[SectionId.Experiments],
     href: `#${SectionId.Experiments}`,
   },
 ]
 
 const AppHeader = () => {
+  const renderNavigationLink = (link: NavigationLink) => (
+    <Link
+      href={link.href}
+      className={styles.navigationLink}
+    >
+      {link.label}
+    </Link>
+  )
+
   return (
     <header className={styles.appHeader}>
       <nav className={styles.navigation}>
-        <ul className={styles.navigationList}>
-          {navigationItems.map(item => (
-            <li key={item.href}>
-              <Link
-                href={item.href}
-                className={styles.navigationLink}
-              >
-                {item.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <List
+          items={navigationLinks}
+          renderItem={renderNavigationLink}
+          className={styles.navigationList}
+        />
       </nav>
     </header>
   )
